a06da0838f17206881c920df55d05b4f3fb8435e
RogerJunior2407 <154432410+GARRINCHAROMARIO@users.noreply.github.com>
2026-08-03 10:01:20 +0200

    force render db

--- CBS/settings.py
+++ CBS/settings.py
@@ -89,16 +89,23 @@ WSGI_APPLICATION = 'CBS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 import dj_database_url
-DATABASES = {
-    'default': 
-        dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'postgres://postgres:postgres@localhost:5432/your_local_db'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-    
-}
-
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    # Fallback for local development on your machine
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
